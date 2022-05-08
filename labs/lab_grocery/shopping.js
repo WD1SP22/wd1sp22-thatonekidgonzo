6e776909b17a0a163735f4ade070066364231d0f
Ubuntu <ubuntu@ip-172-31-69-54.ec2.internal>
2022-05-08 00:20:49 +0000

    grocery lab

--- labs/lab_grocery/shopping.js
+++ labs/lab_grocery/shopping.js
@@ -1,10 +1,10 @@
 /*
  * Filename:    shopping.js
- * Description: FILL IN
- * Author:      FILL IN
+ * Description: Grocery List 
+ * Author:      Gonzalo Garcia
  */
 
-
+'use strict'
 // CREATE YOUR GLOBAL GROCERY LIST ARRAY
 const list = [];
 
@@ -12,7 +12,27 @@ const list = [];
 
 
 // Event Listener to Process Form Submission
+const elForm = document.getElementById('grocery_form');
+elForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+const elText = document.getElementById('texts');
+alert(elText.value);
+list.push(elText.value);
 
+const elNumber = document.getElementById('number');
+alert(elNumber.value);
+list.push(elNumber.value);
+
+const elSelect = document.getElementById('category');
+let currentValue = elSelect.options[elSelect.selectedIndex].value;
+alert(currentValue);
+list.push(elSelect.value);
+
+Update(list);
+
+elForm.reset();
+});
 
 // Event Listener to Handle Removal of First Item
 
@@ -22,3 +42,11 @@ const list = [];
 
 // Function to Update the Text of the List
 
+function Update(list) {
+  const elUL = document.querySelector('ul');
+  let htmlString = '';
+  for (let value of list) {
+      htmlString += '<li>${value}</li>';
+  } 
+  elUL.innerHTML = htmlString;
+}
