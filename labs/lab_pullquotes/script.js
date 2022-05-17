75471e467ad61937920d37fc20e9bb44ac758063
Ubuntu <ubuntu@ip-172-31-69-54.ec2.internal>
2022-05-17 21:27:36 +0000

    Adding all files

---
+++ labs/lab_pullquotes/script.js
@@ -0,0 +1,18 @@
+/*
+ * Filename:    script.js
+ * Description: pullquotes
+ * Author:      Gonzalo Garcia
+ */
+
+'use strict'
+
+/* Getting items <q>*/
+const items = document.getElementsByTagName('<q>');
+
+
+function pullquote() {
+    var create_quote = document.createElement("BLOCKQUOTE");
+    document.getElementById("BLOCKQUOTE").classList.add('pullquote');
+    
+    
+}
