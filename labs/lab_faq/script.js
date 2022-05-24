aa96c64ef4ef167266a2cff2e777c76f7ccc8a57
Ubuntu <ubuntu@ip-172-31-69-54.ec2.internal>
2022-05-24 06:51:16 +0000

    FAQ

--- labs/lab_faq/script.js
+++ labs/lab_faq/script.js
@@ -28,16 +28,41 @@ faq.push(new Info(elQuestion.value, elAnswer.value));
 Update();
 
 });
-      const button = document.createElement('button');
-      button.innerText = 'Toggle Answer';
+      //const button = document.createElement('button');
+      //button.innerText = 'Toggle Answer';
 
 function Update() {
   const elOL = document.querySelector('ol');
   let htmlString = '';
   for (let value of faq) {
-      htmlString += `<li><h3>${value.textone}</h3></li>`;
-      document.body.appendChild(button);
-      htmlString += `<h4>${value.answer}</h4>`;
+      htmlString += `<li>
+                       <h3>${value.textone}</h3>
+                       <button onclick="toggle();">Toggle Answer</button>
+                       <h4 hidden>${value.answer}</h4>
+                    </li>`;
   } 
   elOL.innerHTML += htmlString;
 }
+
+/* FAQ ends here */
+
+/* Event listener for clicking */
+
+let element = document.getElementById("button");
+let hidden = element.getAttribute("hidden");
+
+
+for (let el of element) {
+    element.addEventListener('click', function() {
+       toggle(); 
+    });
+}
+
+function toggle() {
+    if (hidden) {
+        element.removeAttribute("hidden");
+    } else {
+        element.setAttribute("hidden", "hidden");
+    }
+}
+
