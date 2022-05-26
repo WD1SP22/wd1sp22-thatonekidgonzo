e676e7949e7004a893497feae760d2fd0f65d7a6
Ubuntu <ubuntu@ip-172-31-69-54.ec2.internal>
2022-05-26 21:50:30 +0000

    javscirpt

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,11 @@
-'use strict'
+'use strict';
 
 const elClick = document.getElementById('click');
 const colors = ['Egyptian', 'Teal', 'Air Force', 'Navy', 'Tiffany', 'black'];
 let color = 0;
 elClick.addEventListener('click', function() {
- elClick.style.backgroundColor = colors[color++];
- if (color === colors.length) {
-     color = 0;
- }
+  this.style.backgroundColor = colors[color++];
+  if (color === colors.length) {
+    color = 0;
+  }
 });
